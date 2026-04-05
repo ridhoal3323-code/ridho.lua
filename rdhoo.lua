131c7b11d0ce50399a746bca9d8690af9e6d46d3
ridhoal3323-code <ridhoal3323@gmail.com>
2026-04-05 08:38:18 +0700

    rdhoo.lua

--- rdhoo.lua
+++ rdhoo.lua
@@ -1,46 +1,13 @@
 script_key="IbpKKZhEZkEvxnmGXKmTZykBzCSyZJFj";
-getgenv().SailorPieceConfig = getgenv().SailorPieceConfig or {
-    OpenChests       = {
-        "Common Chest",
-        "Rare Chest",
-        "Epic Chest",
-        "Legendary Chest",
+getgenv().SailorVIP = {
+    Optimization = {
+        Enable = true,
+        FPSCap = 10,
     },
-    AutoAscend = true,
-    AutoUpgradeAscend = true,
-    Debug            = false,
-    Performance = {
-        AutoRejoinIfHighRam = 5000,
-        WebhookURL      = "https://discord.com/api/webhooks/1489641574819172392/SiSIuDpsjEM_OJMzz6W9KUghNvV13qaShF6HQigu0jcPDrR4pwVCR3BdbjrgFUWFXlvd",
-        FPSBoost        = false,
-        FPSLock         = 10,
-    },
-    SkillTree = {
-        Auto            = true,
-        Priority        = {"Luck", "Damage", "CritChance", "CritDamage", "HP"},
-    },
-    AutoStrongestMelee = true,
-    Reroll = {
-        Trait = {
-            Auto            = true,
-            TargetRarity    = "Secret",
-        },
-        TargetRace      = {"Kitsune"},
-        TargetClan      = {"Eminence", "Monarch"},
-        },
-    Artifact = {
-        AutoEquip       = true,
-        AutoUpgrade     = true,
-        AutoDeleteTrash = true,
-        UpgradeSet      = {"Celestial Rupture"},-- Ashen Chronicles, Crownless Regalia, Black Horizon, Void Reaver, Celestial Rupture, Abyssal Crown
-        UpgradeStopAt   = 15,
-        TrashRarities   = {"Common", "Rare", "Epic"},
+
+    Webhook = {
+        Enable = false, 
+        URL = "https://discord.com/api/webhooks/1489641574819172392/SiSIuDpsjEM_OJMzz6W9KUghNvV13qaShF6HQigu0jcPDrR4pwVCR3BdbjrgFUWFXlvd", 
     },
 }
-while true do
-    local success = pcall(function()
-        loadstring(game:HttpGet("https://api.luarmor.net/files/v4/loaders/1169527463a6fb002d07c345110bc0aa.lua"))()
-    end)
-    if success then break end
-    task.wait(5)
-end
+loadstring(game:HttpGet("https://api.luarmor.net/files/v4/loaders/a8255d759144bb6127c5b72221cce5cd.lua"))()
