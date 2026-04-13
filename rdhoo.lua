fa18e015f3153b5911c0ea5085cb414fae01cc56
ridhoal3323-code <ridhoal3323@gmail.com>
2026-04-13 18:09:52 +0700

    Update rdhoo.lua

--- rdhoo.lua
+++ rdhoo.lua
@@ -1,13 +1,27 @@
 script_key="IbpKKZhEZkEvxnmGXKmTZykBzCSyZJFj";
-getgenv().SailorVIP = {
-    Optimization = {
-        Enable = true, -- On/Off toàn bộ optimization script
-        FPSCap = 10, -- Số FPS cần lock (ví dụ: 10, 15, 30, 60)
+getgenv().AutoTradeConfig = {
+    -- Danh sách acc main (display name hoặc username) - hỗ trợ nhiều acc
+    MainAccounts = {
+        "RIDHOxN1KA",
+        "rdhoall2",
     },
 
-    Webhook = {
-        Enable = true, -- On/Off gửi webhook thông báo crate
-        URL = "", 
+    -- Danh sách tên item muốn trade (chỉ cần tên, tự detect category + max quantity)
+    Items = {
+        "Clan Reroll",
+        "Mythical Chest",
+        "Aura Crate",
+        "Cosmetic Crate",
+        "Void Fragment",
+        "Limitless Ring",
+        "Dismantle Fang",
+        "Dark Ring",
+        "Reiatsu Core",
+        "Blood Ring",
+        "Frost Relic",
+        "Divine Grail",
+
+
     },
 }
-loadstring(game:HttpGet("https://api.luarmor.net/files/v4/loaders/a8255d759144bb6127c5b72221cce5cd.lua"))()
+loadstring(game:HttpGet("https://api.luarmor.net/files/v4/loaders/75c7fe88bf77410a404199a69629aae3.lua"))()
